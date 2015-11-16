a07ba3f608ede913f951b5b581a8137813795425
guwan <wu6789@qq.com>
2015-11-17 01:05:38 +0800

    add some code

--- src/main/java/com/guwan/support/Tools.java
+++ src/main/java/com/guwan/support/Tools.java
@@ -1,10 +1,21 @@
 package com.guwan.support;
 
+import java.util.ArrayList;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import com.guwan.domain.Authority;
+
 public class Tools {
 
 	public static void main(String[] args) {
 		BCryptEncoder bce = new BCryptEncoder();
 		System.out.println(bce.encode("password"));
+		
+		ArrayList<SimpleGrantedAuthority> sga =new ArrayList<SimpleGrantedAuthority>();		
+		ArrayList<Authority> a =new ArrayList<Authority>();
+		sga.add(new SimpleGrantedAuthority("ADMIN"));
+		a.add(new Authority("admin", "ADMIN"));
 	}
 
 }
